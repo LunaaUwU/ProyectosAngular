28aac5a1b6527c440aa812f6b05d13dae806680c
Luna <lunyyuwu@gmail.com>
2025-11-12 08:45:02 +0100

    Cambios Reactividad

--- proyectoReactividad/src/app/app.routes.ts
+++ proyectoReactividad/src/app/app.routes.ts
@@ -3,9 +3,7 @@ import { ParentComponent } from './components/parent/parent.component';
 import { ChildComponent } from './components/child/child.component';
 
 export const routes: Routes = [
-  { path: 'parent', component: ParentComponent },
-  
-  { path: 'child', component: ChildComponent }, 
+  { path: '', component: ChildComponent },
 
-  { path: '', redirectTo: 'parent', pathMatch: 'full' }
+  // { path: 'parent', component: ParentComponent },
 ];
